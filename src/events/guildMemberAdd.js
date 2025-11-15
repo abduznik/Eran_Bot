12b84fc8978df537774956e8df9238b91a2c73d7
abduznik <85239936+abduznik@users.noreply.github.com>
2025-11-15 10:21:07 +0200

    Update guildMemberAdd.js

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -25,7 +25,7 @@ const getWelcomeMessage = (userId) => {
   */
   return {
     content: `Welcome ${userMention(userId)},
-    Hope you have great time here!
+    מה זה מי זה פה מי הגיע שיאה אללה
   `,
   };
 };
